4847a1f1a2a97dd5b2cbc3b91a787c39e02f2f0a
Sıla <silatekin01@gmail.com>
2023-03-21 22:09:58 +0300

    Update Dribble.cs
    refactoring

--- KickoffStudios/Assets/Scripts/Dribble.cs
+++ KickoffStudios/Assets/Scripts/Dribble.cs
@@ -2,65 +2,83 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class Dribble : MonoBehaviour
 {
-    public float speed = 5.0f;
-    public float shootForce = 10.0f;
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float shootForce = 10f;
 
     private Rigidbody playerRb;
-    private bool ballAttached = false;
     private Rigidbody ballRb;
     private Vector3 ballOffset;
 
-    void Start()
+    private bool ballAttached;
+
+    private void Start()
     {
         playerRb = GetComponent<Rigidbody>();
     }
 
-    void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Ball"))
         {
-            ballAttached = true;
-            ballRb = other.GetComponent<Rigidbody>();
-            ballRb.useGravity = false;
-            ballRb.isKinematic = true;
-            ballOffset = transform.position - ballRb.transform.position;
+            AttachBall(other);
         }
     }
 
-    void Update()
+    private void Update()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
-        Vector3 moveDirection = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        Vector3 moveDirection = new Vector3(moveHorizontal, 0f, moveVertical);
 
-        if (moveDirection != Vector3.zero)
-        {
-            transform.rotation = Quaternion.LookRotation(moveDirection);
-        }
+        RotatePlayer(moveDirection);
 
         if (ballAttached)
         {
             ballRb.MovePosition(transform.position - ballOffset);
         }
 
-        playerRb.MovePosition(playerRb.position + moveDirection * speed * Time.deltaTime);
+        MovePlayer(moveDirection);
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            if (ballAttached)
-            {
-                Vector3 shootDirection = transform.forward;
-                ballRb.isKinematic = false;
-                ballRb.useGravity = true;
-                ballAttached = false;
-                ballRb.AddForce(shootDirection * shootForce, ForceMode.Impulse);
-                ballRb = null;
-            }
+            ShootBall();
         }
     }
-}
 
+    private void AttachBall(Collider other)
+    {
+        ballAttached = true;
+        ballRb = other.GetComponent<Rigidbody>();
+        ballRb.useGravity = false;
+        ballRb.isKinematic = true;
+        ballOffset = transform.position - ballRb.transform.position;
+    }
+
+    private void RotatePlayer(Vector3 moveDirection)
+    {
+        if (moveDirection != Vector3.zero)
+        {
+            transform.rotation = Quaternion.LookRotation(moveDirection);
+        }
+    }
+
+    private void MovePlayer(Vector3 moveDirection)
+    {
+        playerRb.MovePosition(playerRb.position + moveDirection * speed * Time.deltaTime);
+    }
+
+    private void ShootBall()
+    {
+        if (!ballAttached) return;
+
+        Vector3 shootDirection = transform.forward;
+        ballRb.isKinematic = false;
+        ballRb.useGravity = true;
+        ballAttached = false;
+        ballRb.AddForce(shootDirection * shootForce, ForceMode.Impulse);
+        ballRb = null;
+    }
+}
