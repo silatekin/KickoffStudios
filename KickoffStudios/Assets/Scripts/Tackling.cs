e4d460177bedc82c2ad4c5814da40d940c7ede3b
Merdan Yurtsever <merdanm.my@gmail.com>
2023-03-21 22:35:06 +0300

    DRY added to Tackling

--- KickoffStudios/Assets/Scripts/Tackling.cs
+++ KickoffStudios/Assets/Scripts/Tackling.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class Tackling : MonoBehaviour
 {
+    //seperated the definitions
+    [SerializedField] private GameObject Ball;
+    [SerializedField] private float tackleForce = 500f;
+
     public GameObject Ball;
+    //collision
     void OnCollisionEnter(Collision collision)
     {
-        if(collision.gameObject == Ball)
+        if(IsCollidingWithBall(collision))
         {
             Rigidbody BallRigidbody = Ball.GetComponent<Rigidbody>();
-            Vector3 force = transform.forward * 500; 
-            BallRigidbody.AddForce(force);
         }
+    }
+    //isolated the collision
+    private bool IsCollidingWithBall(Collision collision)
+    {
+        return collision.gameObject == Ball;
+    }
+    //isolated force calculation and application
+    private void ApplyTackleForce()
+    {
+        Vector3 force = transform.forward * tackleForce;
+        BallRigidbody.AddForce(force, ForceMode.Impulse);
     }
     // Start is called before the first frame update
     void Start()
