ce2f83507d23ae628b6b65b0a52dbc8b73f7942b
Merdan Yurtsever <merdanm.my@gmail.com>
2023-03-29 03:36:27 +0300

    Tackling fixed (needs opponent)

--- KickoffStudios/Assets/Scripts/Tackling.cs
+++ KickoffStudios/Assets/Scripts/Tackling.cs
@@ -4,25 +4,62 @@ using UnityEngine;
 
 public class Tackling : MonoBehaviour
 {
-    public GameObject Ball;
-    void OnCollisionEnter(Collision collision)
+    //seperated the definitions
+    [SerializeField] private float tackleForce = 1000f;
+    [SerializeField] private string tackleInput = "z";
+
+    /*
+    private void Awake()
     {
-        if(collision.gameObject == Ball)
+        ballRigidbody = ball.GetComponent<Rigidbody>();
+    }
+    */
+
+    //collision
+    
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(IsCollidingWithOpponent(collision) && IsButtonPressed())
         {
-            Rigidbody BallRigidbody = Ball.GetComponent<Rigidbody>();
-            Vector3 force = transform.forward * 500; 
-            BallRigidbody.AddForce(force);
+            ApplyTackleForce(collision);
         }
     }
+
+    //isolated the collision
+    private bool IsCollidingWithOpponent(Collision collision)
+    {
+        return (collision.gameObject.CompareTag("Opponent"));
+    }
+    
+    //isolated force calculation and application
+    private void ApplyTackleForce(Collision collision)
+    {
+        //calculate the force vector from opponent to the player
+        Vector3 forceDirection = transform.position - collision.gameObject.transform.position;
+        forceDirection.Normalize();
+        Vector3 force = forceDirection * tackleForce;
+
+        //Apply force to opponent
+        Rigidbody opponentRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+        opponentRigidbody.AddForce(force, ForceMode.Impulse);
+
+    }
+    private bool IsButtonPressed()
+    {
+        return Input.GetButtonDown(tackleInput);
+    }
+    //not used
+    /*
     // Start is called before the first frame update
     void Start()
     {
         
     }
+    */
 
     // Update is called once per frame
     void Update()
     {
-        
+       // ApplyTackleForce(IsButtonPressed() && IsCollidingWithOpponent());
     }
 }
