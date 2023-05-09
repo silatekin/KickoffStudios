414b8721ce053da847f9a646714d87ec7f9914f8
medetkan-kutlu <20170808068@ogr.akdeniz.edu.tr>
2023-05-09 23:54:26 +0300

    Character models

--- KickoffStudios/Assets/Deneme/MouseLook.cs
+++ KickoffStudios/Assets/Deneme/MouseLook.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun;
 using UnityEngine;
 
 public class MouseLook : MonoBehaviour
@@ -17,14 +18,15 @@ public class MouseLook : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+    {   
+            float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+            float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
-        xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+            xRotation -= mouseY;
+            xRotation = Mathf.Clamp(xRotation, -90f, 90f);
         
-        transform.localRotation = Quaternion.Euler(xRotation, 0f,0f);
-        playerBody.Rotate(Vector3.up * mouseX);
+            transform.localRotation = Quaternion.Euler(xRotation, 0f,0f);
+            playerBody.Rotate(Vector3.up * mouseX);
+            
     }
 }
